e86177ba14bf48f40004424f183421286c15c8c9
Rolan111 <97992969+Rolan111@users.noreply.github.com>
2023-10-24 12:05:17 -0500

    Primera versión - Mundo Folclor

--- Assets/Scripts/Db/Connection.cs
+++ Assets/Scripts/Db/Connection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using MySql.Data.MySqlClient;
 using Unity.VisualScripting;
-using UnityEditor.MemoryProfiler;
+//using UnityEditor.MemoryProfiler;
 using System;
 
 public static class Connection
